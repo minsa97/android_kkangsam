f0cff67ad2ac9cf3dee0dfc6301c1f6222f73f01
mct <minsa97@naver.com>
2022-08-05 18:56:48 +0900

    lab6 Test2Activity - xml 코드로 화면 구성 xml 을 작성해서, 앞 결과와 같게 나오게 한다.

---
+++ app/src/main/java/com/mctlhg/kkangsam/lab6/Test2Activity.kt
@@ -0,0 +1,12 @@
+package com.mctlhg.kkangsam.lab6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mctlhg.kkangsam.R
+
+class Test2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test2)
+    }
+}
