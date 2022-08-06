8e417a6d6a0a8276d7f0c36b9bd1ed384956d302
mct <minsa97@naver.com>
2022-08-06 10:06:32 +0900

    lab7 Telephone_keypad 전화번호 키패드 뷰 실습

---
+++ app/src/main/java/com/mctlhg/kkangsam/lab7/Telephone_keypad.kt
@@ -0,0 +1,12 @@
+package com.mctlhg.kkangsam.lab7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mctlhg.kkangsam.R
+
+class Telephone_keypad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_telephone_keypad)
+    }
+}
