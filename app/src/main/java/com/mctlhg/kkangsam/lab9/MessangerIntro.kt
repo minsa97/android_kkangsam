03bf3f4b4f544dff92d852f2ca88d3b9b8f20c30
mct <minsa97@naver.com>
2022-08-09 11:01:59 +0900

    lab9 MessangerIntro 매신저인트로 화면, 한영, 화면회전

---
+++ app/src/main/java/com/mctlhg/kkangsam/lab9/MessangerIntro.kt
@@ -0,0 +1,12 @@
+package com.mctlhg.kkangsam.lab9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mctlhg.kkangsam.R
+
+class MessangerIntro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messanger_intro)
+    }
+}
