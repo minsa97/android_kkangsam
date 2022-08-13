ea7dd73f429b3cb9655ea291d959a24a88f68c41
mct <minsa97@naver.com>
2022-08-13 09:14:20 +0900

    lab10 KakaoNotificationTest 카카오톡 알림테스트(Notification)

--- app/src/main/java/com/mctlhg/kkangsam/lab10/KakaoNotificationTest.kt
+++ app/src/main/java/com/mctlhg/kkangsam/lab10/KakaoNotificationTest.kt
@@ -1,12 +1,86 @@
 package com.mctlhg.kkangsam.lab10
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 import com.mctlhg.kkangsam.R
+import com.mctlhg.kkangsam.databinding.ActivityKakaoNotificationTestBinding
 
 class KakaoNotificationTest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kakao_notification_test)
+        //setContentView(R.layout.activity_kakao_notification_test)
+
+        val binding = ActivityKakaoNotificationTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.notificationButton.setOnClickListener {
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            val builder: NotificationCompat.Builder
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                val channelId="one-channel"
+                val channelName="My Channel One"
+                val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply {
+                    description = "My Channel One Description"
+                    setShowBadge(true)
+                    val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                    val audioAttributes = AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .setUsage(AudioAttributes.USAGE_ALARM)
+                        .build()
+                    setSound(uri, audioAttributes)
+                    enableVibration(true)
+                }
+                manager.createNotificationChannel(channel)
+                builder = NotificationCompat.Builder(this, channelId)
+            }else {
+                builder = NotificationCompat.Builder(this)
+            }
+
+            builder.run {
+                setSmallIcon(R.drawable.small)
+                setWhen(System.currentTimeMillis())
+                setContentTitle("홍길동")
+                setContentText("안녕하세요.")
+                setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.big))
+            }
+
+            val KEY_TEXT_REPLY = "key_text_reply"
+            var replyLabel = "답장"
+            var remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY).run {
+                setLabel(replyLabel)
+                build()
+            }
+            val replyIntent = Intent(this, ReplyReceiver::class.java)
+            val replyPendingIntent = PendingIntent.getBroadcast(
+                this, 30, replyIntent, PendingIntent.FLAG_MUTABLE
+            )
+
+            builder.addAction(
+                NotificationCompat.Action.Builder(
+                    R.drawable.send,
+                    "답장",
+                    replyPendingIntent
+                ).addRemoteInput(remoteInput).build()
+            )
+
+            manager.notify(11, builder.build())
+
+        }
     }
 }
---
+++ app/src/main/java/com/mctlhg/kkangsam/lab10/ReplyReceiver.kt
@@ -0,0 +1,24 @@
+package com.mctlhg.kkangsam.lab10
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.RemoteInput
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val replyTxt = RemoteInput.getResultsFromIntent(intent)
+            ?.getCharSequence("key_text_reply")
+        Log.d("kkangtest", "replyTxt:$replyTxt")
+
+        val manager = context.getSystemService(
+            AppCompatActivity.NOTIFICATION_SERVICE
+        ) as NotificationManager
+
+        manager.cancel(11)
+    }
+}
